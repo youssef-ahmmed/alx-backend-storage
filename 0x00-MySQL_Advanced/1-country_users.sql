934a5c8c722ac0129409e9d3afbdf5d3ff356bf2
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-18 13:42:23 +0200

    Task1[Add]: alter table users with enum data type

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a table users
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INTEGER AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    `country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (`id`)
+);
