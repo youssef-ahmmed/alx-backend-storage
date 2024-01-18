8165f7bd4ee826bbd18fdb186ff4cc6a5ad3cb6d
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-18 14:42:29 +0200

    Task4[Add]: creates a trigger that decreases the quantity of an item after adding a new order

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a trigger that decreases the quantity of an item after adding a new order
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON `orders`
+FOR EACH ROW
+BEGIN
+   UPDATE `items`
+   SET `quantity` = `quantity` - NEW.number;
+END;
