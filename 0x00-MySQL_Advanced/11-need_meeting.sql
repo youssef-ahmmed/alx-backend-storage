ff26572c4ad816287bf18a48d05e9630ad129831
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-18 19:30:54 +0200

    Task11[Add]: create a view for students with some conditions

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,9 @@
+-- SQL script that creates a view need_meeting
+CREATE VIEW `need_meeting` AS
+    SELECT `name`
+    FROM `students`
+    WHERE
+        `score` < 80
+        AND
+        (`last_meeting` IS NULL OR DATEDIFF(CURDATE(), `last_meeting`) > 30);
+
