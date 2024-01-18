43d5003882dbb13d1e1474a11408090997b45d8f
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-18 14:25:14 +0200

    Task3[Add]: select all brands with there lifespan with specific style

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT
+    `band_name`, COALESCE(`split`, 2022) - `formed` AS `lifespan`
+FROM
+    `metal_bands`
+WHERE
+    `style` LIKE '%Glam rock%'
+ORDER BY
+    `lifespan` DESC;
