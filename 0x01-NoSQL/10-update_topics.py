56db8c8f9d2a2cda0ac948db5e5fbb5e434cee42
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-23 13:10:03 +0200

    Task10[Add]: Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Change school topics"""
+from typing import List
+
+from pymongo import collection
+
+
+def update_topics(mongo_collection: collection, name: str, topics: List[str]) -> None:
+    """function that changes all topics of a school document based on the name"""
+    mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
