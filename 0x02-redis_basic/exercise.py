628601dd0efe289faa5f4883364bcf6a15bd87c8
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-25 14:38:17 +0200

    Task0[Edit]: Writing strings to Redis {type annotation}

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 """Connecting Redis with python"""
 import uuid
-from typing import Any
+from typing import Union
 
 import redis
 
 
 class Cache:
     """Cache class for connecting to redis"""
-
     def __init__(self) -> None:
         """Init the redis connection"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the given data with random key"""
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
