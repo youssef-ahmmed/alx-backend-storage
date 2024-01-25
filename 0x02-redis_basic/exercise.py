9dab7d63b50cf0b68afd499bab5907c45e34943c
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-25 14:34:17 +0200

    Task0[Edit]: Writing strings to Redis {type docs}

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,12 +7,15 @@ import redis
 
 
 class Cache:
+    """Cache class for connecting to redis"""
 
     def __init__(self) -> None:
+        """Init the redis connection"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Any) -> str:
-        key = str(uuid.uuid4())
+        """Store the given data with random key"""
+        key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
